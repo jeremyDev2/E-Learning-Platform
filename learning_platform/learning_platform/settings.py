603aaa4ac14643c8f66d1634ada943534ea09327
Daniel <danil.malaev3@gmail.com>
2026-04-16 23:42:07 +0300

    add: MEDIA_URL & MEDIA_ROOT var's

--- learning_platform/learning_platform/settings.py
+++ learning_platform/learning_platform/settings.py
@@ -105,3 +105,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# var's for upload media files
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
