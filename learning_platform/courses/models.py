56082be69ef9a2e1c32bc3923752127c66d9ba35
Daniel <danil.malaev3@gmail.com>
2026-04-23 23:25:27 +0300

    add: limit_choices_to parameter

--- learning_platform/courses/models.py
+++ learning_platform/courses/models.py
@@ -37,7 +37,8 @@ class Module(models.Model):
 
 class Content(models.Model):
     module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    #limit_choices_to : filter of object's. model__in taking only collection item's
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'model__in':('text', 'video', 'image', 'file')})
 
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
